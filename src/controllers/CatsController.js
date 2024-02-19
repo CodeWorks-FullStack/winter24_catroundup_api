fc08c2ec32772b4097b350d7dfbbc52cbc2d1c66
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-19 11:56:31 -0700

    got string back from api, things are workin

---
+++ src/controllers/CatsController.js
@@ -0,0 +1,22 @@
+import BaseController from "../utils/BaseController.js";
+
+// NOTE inheriting all members from BaseController (properties and methods)
+export class CatsController extends BaseController {
+  constructor () {
+    // NOTE calls the constructor on BaseController
+    // NOTE label on door in hallway. allows us to send requests to http:localhost:3000/api/cats
+    super('api/cats')
+    // NOTE allows us to set up code to run when requests are sent to this endpoint
+    this.router.get('', this.getCats)
+  }
+
+  /**
+   * Sends all values back to the client
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  getCats(request, response, next) {
+    response.send('This is the cats door, welcome')
+  }
+}
